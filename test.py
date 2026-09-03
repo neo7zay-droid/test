cae62163fa74e9114cb4dd888ecfe170b56cf51a
Neo <neo7zay@gmail.com>
2026-09-03 14:27:52 +0530

    Completed Phase 6: Automated data export to Excel CSV files

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import requests
+import csv
 from bs4 import BeautifulSoup
 url="https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops"
 response=requests.get(url)
@@ -8,5 +9,9 @@ print("___DATA EXTRACTION___")
 print(first_quote)
 print("____________________________")
 all_quotes=soup.find_all("a",class_="title")
-for q in all_quotes:
-    print("Found product: " + q.text)
+with open("laptops.csv","w",newline="",encoding="utf-8")as f:
+    writer=csv.writer(f)
+    writer.writerow("Product Name")
+    for q in all_quotes:
+        print("Found product: " + q.text)
+        writer.writerow([q.text])
