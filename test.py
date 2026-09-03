317ab55b2716885cd268d7c77706d36cc5d5abad
Neo <neo7zay@gmail.com>
2026-09-03 12:51:53 +0530

    polished data labels to display e-commerce products cleanly

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
-url="https://quotes.toscrape.com"
+url="https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops"
 response=requests.get(url)
 soup=BeautifulSoup(response.text, "html.parser")
-first_quote=soup.find("span",class_="text").text
+first_quote=soup.find("a", class_="title")
 print("___DATA EXTRACTION___")
 print(first_quote)
 print("____________________________")
-all_quotes=soup.find_all("span",class_="text")
+all_quotes=soup.find_all("a",class_="title")
 for q in all_quotes:
-    print("Found Quote: " + q.text)
+    print("Found product: " + q.text)
